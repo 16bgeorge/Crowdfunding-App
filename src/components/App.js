bea971702a58417901c2b2f1c75a8891472ef454
Brendon George <brendongeo@brendons-mbp.myfiosgateway.com>
2023-09-13 13:46:11 -0400

    Load Accounts

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,40 @@
-import { Container } from 'react-bootstrap';
+import { useEffect, useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { ethers } from 'ethers'
 
 // Components 
 import Navigation from './Navigation';
+import Info from './Info';
 
 function App() {
+
+	const [account, setAccount] = useState(null)
+
+	// account -> Variable od current account value
+	// setAccount -> function to update account value
+	// null is default value 
+
+	const loadBlockchainData = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+		const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+		const account = ethers.utils.getAddress(accounts[0])
+		setAccount(account)
+
+		// Add to state
+	}
+
+	useEffect(() => {
+		loadBlockchainData()
+	});
+
 	return(
 		<Container>
 			<Navigation />
+			<hr />
+			{account&& (
+				<Info account={account} />
+			)}
 		</Container>
 	)
 }
